6dad86bb66e490c1ceac17789dbf6bf647994580
Dorothy2020 <wangarraakoth@gmail,com>
2024-04-04 17:11:29 +0300

    feat:add url login credentials

--- ligiopen/football/urls.py
+++ ligiopen/football/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
 from .views import index_view
+from . import views
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('pages/login.html/', views.login_view, name='login'),
+    path('pages/signup.html/', views.signup_view, name='signup'),
+
+   
+ 
 ]
+
