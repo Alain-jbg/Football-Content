e65db61a437167664b8b00825a13ef69d2f6d68f
Dorothy2020 <wangarraakoth@gmail,com>
2024-05-22 05:34:34 +0300

    modified:settings file

--- ligiopen/settings.py
+++ ligiopen/settings.py
@@ -146,7 +146,7 @@ LOGIN_REDIRECT_URL = '/admin/dashboard/'  # Replace '/admin/dashboard/' with you
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'wangarraakoth@gmail.com'
-EMAIL_HOST_PASSWORD = 'qjaxpnhoulqoxdoz'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
