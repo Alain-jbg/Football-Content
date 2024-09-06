0778b6ca17663a33e851ab11f2e94fca35a815d5
Dorothy Akoth <wangarraakoth@gmail.com>
2024-09-06 06:31:57 +0300

    github actions

--- ligiopen/settings.py
+++ ligiopen/settings.py
@@ -114,8 +114,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/admin/dashboard/'
 
 # Email settings
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='default-email@example.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.example.com')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
